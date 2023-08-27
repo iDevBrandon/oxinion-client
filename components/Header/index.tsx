f1187fc94b42f0b96173014985c59ca78fd520d3
Brandon Ha <idevbrandon@gmail.com>
2023-08-27 13:37:29 +0900

    insert logo

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,10 +1,23 @@
 "use client";
 
 import React from "react";
-import { HeaderGroup } from "./styles";
+import { HeaderGroup, HeaderNav, StyledLogo } from "./styles";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../../public/images/oxinion_logo.png";
 
 const Header = () => {
-  return <HeaderGroup>Header</HeaderGroup>;
+  return (
+    <HeaderGroup>
+      <HeaderNav>
+        <Link href="/" className="logo">
+          <StyledLogo>
+            <Image src={logo} alt="OXINION_logo" width={40} height={40} />
+          </StyledLogo>
+        </Link>
+      </HeaderNav>
+    </HeaderGroup>
+  );
 };
 
 export default Header;
