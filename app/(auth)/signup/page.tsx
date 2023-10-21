4cd805f09ce5cbeffe36b79c938d5d1f1830253e
Brandon Ha <idevbrandon@gmail.com>
2023-10-22 02:28:58 +0900

    Set up signup UI

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -5,8 +5,14 @@ import React, { useCallback, useState } from "react";
 import { AuthFormContainer } from "./styles";
 
 const Signup = () => {
+  const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const onChangeUsername = useCallback((e: any) => {
+    setUsername(e.target.value);
+  }, []);
 
   const onChangeEmail = useCallback((e: any) => {
     setEmail(e.target.value);
@@ -16,6 +22,10 @@ const Signup = () => {
     setPassword(e.target.value);
   }, []);
 
+  const onChangeConfirmPassword = useCallback((e: any) => {
+    setConfirmPassword(e.target.value);
+  }, []);
+
   return (
     <AuthFormContainer>
       <div>
@@ -23,28 +33,47 @@ const Signup = () => {
       </div>
       <form>
         <div>
+          <label htmlFor="username">Username</label>
+          <br />
+          <input name="username" value={username} onChange={onChangeUsername} />
+        </div>
+        <div>
           <label htmlFor="email">Email</label>
           <br />
           <input name="email" value={email} onChange={onChangeEmail} />
         </div>
         <div>
-          <label htmlFor="email">Password</label>
+          <label htmlFor="password">Password</label>
           <br />
           <input
             type="password"
             name="password"
-            value={email}
+            value={password}
             onChange={onChangePassword}
             required
           />
         </div>
         <div>
-          <Link href="/login">
-            Already have an account?
-            <button>Sign In</button>
-          </Link>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <br />
+          <input
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={onChangeConfirmPassword}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">Complete to login</button>
         </div>
       </form>
+      <div>
+        <Link href="/login">
+          Already have an account?
+          <button>Sign In</button>
+        </Link>
+      </div>
     </AuthFormContainer>
   );
 };
