b0152ae769a9a04dcf6d53e7472a47dd4998742b
Brandon Ha <idevbrandon@gmail.com>
2023-11-04 18:07:03 +0900

    Deploy test

--- constants/api.ts
+++ constants/api.ts
@@ -3,8 +3,9 @@
 //     ? "https://api.oxinion.com/api"
 //     : "http://localhost:8800/api";
 
-export const backUrl =
-  (process.env.NEXT_PUBLIC_NODE_ENV === "development" &&
-    "http://localhost:8800/api") ||
-  (process.env.NEXT_PUBLIC_NODE_ENV === "production" &&
-    "https://api.oxinion.com/api");
+export const backUrl = "https://api.oxinion.com/api";
+// process.env.NEXT_PUBLIC_NODE_ENV === "production" &&
+// "https://api.oxinion.com/api";
+
+// (process.env.NEXT_PUBLIC_NODE_ENV === "development" &&
+// "http://localhost:8800/api") ||
