d44d25292b7e4950e2c5d24a389192617bd33c24
Brandon Ha <idevbrandon@gmail.com>
2023-11-04 23:56:56 +0900

    feat : Conditional render for header

--- apis/auth.ts
+++ apis/auth.ts
@@ -16,3 +16,7 @@ export function signInAPI(data: { email: string; password: string }) {
 export function loadMyInfoAPI(options?: AxiosRequestConfig) {
   return api.get("/users", options).then((response) => response.data);
 }
+
+export function logoutAPI() {
+  return api.post("/auth/logout").then((response) => response.data);
+}
--- components/Common/Dropdown.tsx
+++ components/Common/Dropdown.tsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { GoSignOut } from "react-icons/go";
 import { RiUserSmileLine } from "react-icons/ri";
 import { DropdownWrapper, MenuItem } from "./styles";
+import { useRouter } from "next/navigation";
+import { useMutation } from "@tanstack/react-query";
+import { logoutAPI } from "@/apis/auth";
 
 const Dropdown = () => {
+  const router = useRouter();
+  const { mutate } = useMutation(logoutAPI, {
+    onSuccess: () => {
+      router.push("/");
+    },
+    onError: (error: any) => {
+      alert(error.response?.data);
+    },
+  });
+
+  const signoutHandler = useCallback(() => {
+    mutate();
+  }, [mutate]);
+
   return (
     <DropdownWrapper>
-      <MenuItem onClick={() => console.log("profile")}>
+      <MenuItem onClick={() => router.push("/profile")}>
         <RiUserSmileLine />
         <span>&nbsp;Profile</span>
       </MenuItem>
-      <MenuItem onClick={() => console.log("logout")}>
+      <MenuItem onClick={signoutHandler}>
         <GoSignOut />
         <span>&nbsp;Sign out</span>
       </MenuItem>
