d7813d7a91c83e77024b3eba32c587eab0a21608
Brandon Ha <idevbrandon@gmail.com>
2023-11-04 17:51:22 +0900

    Fix typo

--- constants/api.ts
+++ constants/api.ts
@@ -4,5 +4,7 @@
 //     : "http://localhost:8800/api";
 
 export const backUrl =
-  (process.env.NODE_ENV === "development" && "http://localhost:8800/api") ||
-  (process.env.NODE_ENV === "production" && "https://api.oxinion.com/api");
+  (process.env.NEXT_PUBLIC_NODE_ENV === "development" &&
+    "http://localhost:8800/api") ||
+  (process.env.NEXT_PUBLIC_NODE_ENV === "production" &&
+    "https://api.oxinion.com/api");
