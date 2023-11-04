578c58c26c89010e2f598af7d1bb0fc5038880b4
Brandon Ha <idevbrandon@gmail.com>
2023-11-04 17:43:59 +0900

    Update backUrl config

--- constants/api.ts
+++ constants/api.ts
@@ -1,4 +1,8 @@
+// export const backUrl =
+//   process.env.NEXT_PUBLIC_NODE_ENV === "production"
+//     ? "https://api.oxinion.com/api"
+//     : "http://localhost:8800/api";
+
 export const backUrl =
-  process.env.NEXT_PUBLIC_NODE_ENV === "production"
-    ? "https://api.oxinion.com/api"
-    : "http://localhost:8800/api";
+  (process.env.NODE_ENV === "development" && "http://localhost:8800/api") ||
+  (process.env.NODE_ENV === "production" && "https://api.oxinion.com/api");
